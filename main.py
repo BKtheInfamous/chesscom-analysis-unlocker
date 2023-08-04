9c8bfc42eb24bd0830ecad01b078c6160131931a
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-04 01:55:02 -0700

    first commit. making sure selenium works

---
+++ main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://google.com/")
+
+"""
+# alternatively with auto-close...
+
+import os
+
+os.environ['PATH'] += r"/Users/bk/CS-misc/seleniumDrivers/C2/chromedriver"
+driver = webdriver.Chrome()
+driver.get("https://google.com/")
+
+time.sleep(5)
+
+"""
