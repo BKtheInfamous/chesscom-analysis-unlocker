949e2c6fdc107491fa11eebda0f9a84fbee775b9
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-06 23:13:05 -0700

    final v0.2.3 build: pre-gunicorn

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message) => {
   const url = message.url;
-  fetch('http://120.0.0.1:5002/', {
+  fetch('http://127.0.0.1:5002/', {
     method: 'POST',
     body: JSON.stringify({ url }),
     headers: {
